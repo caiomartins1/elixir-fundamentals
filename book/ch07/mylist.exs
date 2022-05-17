62a069449d0af46ab9ef2cab1e6e894b1f8cfe6e
Caio Martins <dev.caiomartins@gmail.com>
2022-05-17 13:05:20 +0100

    reacreating map function

--- book/ch07/mylist.exs
+++ book/ch07/mylist.exs
@@ -7,8 +7,13 @@ defmodule MyList do
 
   def add([]), do: []
   def add([head | tail]), do: [head + 1 | add(tail)]
+
+  def map([], _func), do: []
+  def map([head | tail], func), do: [func.(head) | map(tail, func)]
 end
 
 IO.puts(MyList.len([1, 2, 3]))
 IO.inspect(MyList.square([1, 2, 3]))
 IO.inspect(MyList.add([0, 1, 2]))
+IO.inspect(MyList.map([0, 1, 2], &(&1 + 1)))
+IO.inspect(MyList.map([0, 1, 2], &(&1 * &1)))
